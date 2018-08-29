9546a598f8fc3b465b8cade340e4126ef7e0ae41
Nicholas Kampe <blindside328@gmail.com>
2018-08-28 23:29:17 -0500

    updates for tplink casa, current devices, adding jenkins and roomba

--- devicetypes/kris2k2/wemo-dimmer-light-switch.src/wemo-dimmer-light-switch.groovy
+++ devicetypes/kris2k2/wemo-dimmer-light-switch.src/wemo-dimmer-light-switch.groovy
@@ -253,7 +253,7 @@ def refresh() {
 }
 
 def subscribe(hostAddress) {
-    debug("Executing 'subscribe()'")
+    debug("Executing subscribe(${hostAddress})")
     def address = getCallBackAddress()
     new physicalgraph.device.HubAction("""SUBSCRIBE /upnp/event/basicevent1 HTTP/1.1
 HOST: ${hostAddress}
@@ -267,6 +267,7 @@ User-Agent: CyberGarage-HTTP/1.0
 }
 
 def subscribe() {
+    debug("calling getHostAddress()")
     subscribe(getHostAddress())
 }
 
@@ -286,7 +287,7 @@ def subscribe(ip, port) {
 }
 
 def resubscribe() {
-    debug("Executing 'resubscribe()'")
+    debug("Executing resubscribe()")
     def sid = getDeviceDataByName("subscriptionId")
 
     new physicalgraph.device.HubAction("""SUBSCRIBE /upnp/event/basicevent1 HTTP/1.1
